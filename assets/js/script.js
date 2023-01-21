2ea81e1a78f541294dcabcb751f2cf8d298b1e84
kamel beloula <kamel.solo@live.com>
2023-01-21 21:37:43 +0000

    added an if section to allocate the right class time to the textarea

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,27 +3,18 @@ $('#currentDay').text(moment().format("dddd, Do MMMM, YYYY"));
 
 let rootEl = $('.container');
 
-var OfficeHours = [
-  '9 am',
-  '10 am',
-  '11 am',
-  '12 pm',
-  '1 pm',
-  '2 pm',
-  '3 pm',
-  '4 pm',
-  '5 pm',
-];
+const OfficeHoursStart = 9; 
+const OfficeHoursEnd = 17;
 
-for (var i = 0; i < OfficeHours.length; i++) {
+for (var hour = OfficeHoursStart; hour <= OfficeHoursEnd; hour++) {
 // Create a new `<div>` for each row
   let rowEl = $('<div>').addClass("row time-block");
 
   // Create a new `<p>` for each row with the text from the array
-  let timeEl = $('<p>').addClass("col-1 hour").text(OfficeHours[i]);
+  let timeEl = $('<p>').addClass("col-1 hour").text(moment(hour, "HH").format("h a"));
 
   // Create a new `<textarea>` for each row
-  let textEl = $("<textarea>").addClass("col-10 description hour").attr("id", OfficeHours[i]);
+  let textEl = $("<textarea>").addClass("col-10 description hour");
 
   // Create a new `<bitton>` for each row
   let saveButtonEl = $('<button>').addClass('col-1 saveBtn');
@@ -34,22 +25,18 @@ for (var i = 0; i < OfficeHours.length; i++) {
 //  Add new `<p>`, `<textarea>`, `<bitton>` to the `<div>` rowEl.
   rowEl.append(timeEl, textEl, saveButtonEl);
 
+  //add different classes to the textElement based on the id
   let currentHour = moment().format("HH");
-  console.log(currentHour);
-  
-  if (currentHour > OfficeHours[i]){
-      textEl.addClass('future');
-  }else if(currentHour < OfficeHours[i]){
-          textEl.addClass('past');
-        }else{
-          textEl.addClass('present');
-        }
+
+  if (currentHour > hour){
+    textEl.addClass('past');
+
+  }else 
+      if(currentHour == hour){
+        textEl.addClass('present');
+
+      }else{
+        textEl.addClass('future');
+      }
 
 }
-
-
-
-// Alternatively use the `$.each` method
-// $.each(abilities, function(i, ability) {
-//    abilityEl.append("<div>" + ability + "</div>");
-// })
